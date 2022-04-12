a5c04637c7c52f3e2361d844debf61c8f7fe667e
iNaXozZ <79304081+iNaXozZ@users.noreply.github.com>
2022-04-13 01:32:47 +0200

    Commentaire bouton retour ListeEnchereEnCoursVue

--- Encheres/Encheres/Vues/ListeEnchereEnCoursVue.xaml.cs
+++ Encheres/Encheres/Vues/ListeEnchereEnCoursVue.xaml.cs
@@ -27,13 +27,22 @@ namespace Encheres.Vues
             Navigation.PushAsync(new PageEnchereVue(current));
         }
 
+        /// <summary>
+        /// Ce bouton permet à l'utilisateur de retourner vers sa page de profil ou la page de connexion de l'application
+        /// suivant si il est connecté ou non.
+        /// </summary>
         private async void OnClickRetourPageProfil(object sender, EventArgs e)
         {
+            //On vérifie si un utilisateur est connecté en cherchant un id d'utilisateur dans le cache de l'application
             _idUser = await  SecureStorage.GetAsync("ID");
+
+            // Si le cache renvoi un id d'utilisateur, on retourne l'utilisateur connecté vers sa page de profil
             if (_idUser != null)
             {
                 await Navigation.PushAsync(new PageProfilVue());
             }
+
+            //Si le cahce ne renvoi pas d'id, on renvoi l'utilisateur vers la page de connexion de l'application
             else
             {
                 await Navigation.PushAsync(new LoginPageVue());
