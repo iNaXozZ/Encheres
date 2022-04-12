6a3f2ce721c295abbdb55c81bb336239bfa331dd
iNaXozZ <79304081+iNaXozZ@users.noreply.github.com>
2022-04-13 01:26:53 +0200

    Commentaire bouton déconnexion PageProfilVue

--- Encheres/Encheres/Vues/PageProfilVue.xaml.cs
+++ Encheres/Encheres/Vues/PageProfilVue.xaml.cs
@@ -21,8 +21,12 @@ namespace Encheres.Vues
 
         private void OnClickDeconnexion(object sender, EventArgs e)
         {
+            //Lors de l'appuie sur le bouton de déconnexion, on efface l'id et le pseudo de l'utilisateur
+            //présent dans le cache de l'application.
             SecureStorage.Remove("ID");
             SecureStorage.Remove("PSEUDO");
+
+            //On redirige l'utilisateur vers la page de connexion de l'application
             Navigation.PushAsync(new LoginPageVue());
         }
 
