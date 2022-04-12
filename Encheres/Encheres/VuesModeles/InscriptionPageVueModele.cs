650ae4b97f98bf7c2d100636a4c21fb735ecb8eb
iNaXozZ <79304081+iNaXozZ@users.noreply.github.com>
2022-04-13 01:21:28 +0200

    Méthode InscriptionPageVueModele

--- Encheres/Encheres/VuesModeles/InscriptionPageVueModele.cs
+++ Encheres/Encheres/VuesModeles/InscriptionPageVueModele.cs
@@ -96,29 +96,47 @@ namespace Encheres.VuesModeles
         #endregion
 
         #region Méthodes
+
+        /// <summary>
+        /// Cette méthode permet via la récupération des informations émis du formulaire d'inscription, d'inscrire
+        /// un utilisateur dans la base de données en interrogeant l'API. 
+        /// </summary>
         public  void ActionPageRegistration()
         {
+            //On instancie un utilisateur avec les informations issues du formulaire d'inscription
             User unUser = new User(Email, Photo, Password , Pseudo);
+
             Task.Run(async () =>
             {
+                // On demande à l'API de valider l'inscription de l'utilisateur ou non
                 if (await _apiServicesRegistration.PostRegistrationAsync(unUser, "api/postUser"))
                 {
                     auth = true;
                     User.CollClasse.Add(unUser);
+
+                    //On vérifie que l'utilisateur a bien un email, un mot de passe et un pseudo
                     if (unUser.Email != null && unUser.Password != null && unUser.Pseudo != null )
                     {
+                        // Si toutes les conditions sont réunies, on valide l'inscription et la connexion de l'utilisateur
+                        //en le renvoyant sur sa page de profil. A Modif !!!!!
                         Device.BeginInvokeOnMainThread(() =>
                         {
                             Application.Current.MainPage = new NavigationPage(new ListeEnchereEnCoursVue());
                         });
                     }
+
+                    // Si tous les champs ne sont pas remplis, on affiche un message d'erreur lui indiquant qu'il faut revoir ses champs
+                    //d'inscription
                     else
                     {
                         auth = false;
-                        await App.Current.MainPage.DisplayAlert("Erreur", "Veuillez Revoir les vos champs", "OK");
+                        await App.Current.MainPage.DisplayAlert("Erreur", "Veuillez Revoir vos champs", "OK");
                     }
 
                 }
+
+                // Si problème lors de l'envoi de la demande vers l'API, afficher un message d'erreur demandant si il y a un 
+                //problème avec l'API ?
                 else
                 {
                     await App.Current.MainPage.DisplayAlert("Echec", "Problème de synchronisation avec le serveur ? ", "OK");
